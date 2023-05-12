3e82e4198e6812dfd19903e360c942982372e6d5
Xu Qian <1464238196@qq.com>
2023-05-12 23:47:09 +0800

    ✨ feat(每日反馈): 修改每日反馈形式

--- src/pages/ActivityAdmin/index.tsx
+++ src/pages/ActivityAdmin/index.tsx
@@ -83,12 +83,16 @@ const ActivityAdmin: React.FC = () => {
   };
 
   const handleRefundModalOk = async (activityId: number, userId: number) => {
-    const result = await refundOne(activityId, userId);
-    console.log(result);
-    if (!!result && result.data.success) {
-      message.success('提交退款成功');
-    }
+    const refund = async () => {
+      const result = await refundOne(activityId, userId);
+      console.log(result);
+      if (!!result && result.data.success) {
+        message.success('提交退款成功');
+      }
+    };
+    await refund();
     setIsRefundModalOpen(false);
+    actionRef.current?.reload()
   };
 
   const showRefundModal = (record: MatchResultItem) => {
@@ -976,6 +980,9 @@ const ActivityAdmin: React.FC = () => {
           }}
           activity={currentReqActivity}
           values={currentRow}
+          reload={() => {
+            actionRef.current?.reload();
+          }}
         />
       )}
       {dailyQuestionDrawerVisible && (
