dbe7d27049096cf4da4efe64576ced5f9017cfd3
Xu Qian <1464238196@qq.com>
2023-05-12 15:17:38 +0800

    ✨ feat(活动): 修改头图

--- src/pages/ActivityCreation/Card.tsx
+++ src/pages/ActivityCreation/Card.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 import styles from './index.less';
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
-const Card = ({ term, startDate, endDate, editActivity }) => {
+const Card = ({ activity, editActivity }) => {
   return (
     <div>
       <div className={styles.card}>
         <div className={styles.top}>
           {/* <div className={styles.alert}>已完成</div> */}
           <div>
-            第 <b>{term}</b> 期
+            第 <b>{activity.term}</b> 期
           </div>
         </div>
         <div className={styles.time}>
           <div>活动时间</div>
           <div className={styles.date}>
-            {startDate} ~ <br />
-            {endDate}
+            {activity.startDate} ~ <br />
+            {activity.endDate}
           </div>
         </div>
         <div className={styles.iconcontainer}>
           {/*<DeleteOutlined className={styles.icon} onClick={() => handleDeleteActivity(term)} />*/}
-          <EditOutlined className={styles.icon} onClick={editActivity}/>
+          <EditOutlined className={styles.icon} onClick={() => {editActivity(activity)}}/>
         </div>
       </div>
     </div>
